7b2442cd8039d267ad1fd5bf72d5f8c8c81028c1
VictorGianelli <victorgnll@gmail.com>
2025-02-02 11:08:45 -0300

    Header Params

--- src/main/java/com/terabyte/primeiro_projeto_spring_boot/PrimeiraController.java
+++ src/main/java/com/terabyte/primeiro_projeto_spring_boot/PrimeiraController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 
 
@@ -35,6 +36,16 @@ public class PrimeiraController {
     @PostMapping("/metodoComBodyParams")
     public String metodoComBodyParams(@RequestBody Usuario usuario) {
         return "metodoComBodyParams " + usuario.username;
+    }
+    
+    @PostMapping("/metodoComHeaders")
+    public String metodoComHeaders(@RequestHeader("name") String name) {
+        return "metodoComHeaders " + name;
+    }
+
+    @PostMapping("/metodoComHeaders2")
+    public String metodoComHeaders2(@RequestHeader Map<String,String> headers) {
+        return "metodoComHeaders " + headers.entrySet();
     }
     
     record Usuario(String username){}
